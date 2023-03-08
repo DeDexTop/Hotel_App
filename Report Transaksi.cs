6fc425fa800a9749fdd0d073b16ee6421b95bf38
Arief Aliman <4r1efaliman@gmail.com>
2023-03-08 22:44:25 +0700

    final update

--- Report Transaksi.cs
+++ Report Transaksi.cs
@@ -32,7 +32,7 @@ namespace Hotel_App
 
         private void Report_Transaksi_Load(object sender, EventArgs e)
         {
-            func.ReadData("SELECT tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id", dgv_Transaksi);
+            func.ReadData("SELECT user.nama Kasir, tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id JOIN user ON tb_transaksi.id_user = user.id", dgv_Transaksi);
         }
 
         public string Harga()
@@ -48,7 +48,7 @@ namespace Hotel_App
 
         void SearchTanggal()
         {
-            string query = "SELECT tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id WHERE tb_transaksi.dibuat BETWEEN @startDate AND @endDate";
+            string query = "SELECT user.nama Kasir,tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id JOIN user ON tb_transaksi.id_user = user.id WHERE tb_transaksi.dibuat BETWEEN @startDate AND @endDate";
             MySqlCommand command = new MySqlCommand(query, koneksi);
             command.Parameters.AddWithValue("@startDate", dt_Tanggal.Value.ToString("yyyy-MM-dd"));
             command.Parameters.AddWithValue("@endDate", dt_Tanggal2.Value.ToString("yyyy-MM-dd"));
@@ -60,7 +60,7 @@ namespace Hotel_App
         }
         void Search()
         {
-            string query = "SELECT tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id WHERE tb_transaksi.dibuat BETWEEN @startDate AND @endDate AND tb_transaksi.nama_pelanggan Like '" + txt_Search.Text + "%'";
+            string query = "SELECT user.nama Kasir,tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id JOIN user ON tb_transaksi.id_user = user.id WHERE tb_transaksi.dibuat BETWEEN @startDate AND @endDate AND tb_transaksi.nama_pelanggan Like '" + txt_Search.Text + "%'";
             MySqlCommand command = new MySqlCommand(query, koneksi);
             command.Parameters.AddWithValue("@startDate", dt_Tanggal.Value.ToString("yyyy-MM-dd"));
             command.Parameters.AddWithValue("@endDate", dt_Tanggal2.Value.ToString("yyyy-MM-dd"));
@@ -86,7 +86,7 @@ namespace Hotel_App
         private void btn_Batal_Click(object sender, EventArgs e)
         { 
             txt_Search.Text = string.Empty;
-            func.ReadData("SELECT tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id", dgv_Transaksi);
+            func.ReadData("SELECT user.nama Kasir, tb_transaksi.nama_pelanggan Nama, detail_transaksi.checkin CheckIn, detail_transaksi.checkout CheckOut, tb_kamar.tipe_kamar Tipe, tb_kamar.harga_per_malam Harga, detail_transaksi.jumlah_kamar Jumlah_Kamar, detail_transaksi.harga Harga_Total, tb_transaksi.dibuat Tanggal FROM detail_transaksi JOIN tb_transaksi ON detail_transaksi.id_transaksi = tb_transaksi.id JOIN tb_kamar ON detail_transaksi.id_kamar = tb_kamar.id JOIN user ON tb_transaksi.id_user = user.id", dgv_Transaksi);
         }
 
         private void btn_Print_Click(object sender, EventArgs e)
